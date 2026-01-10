bbf03908cbe8ab94a936fda37e4262409bcae6d1
mongsil <wkdgustjrrpdla@naver.com>
2026-01-10 21:10:08 +0900

    initial commit

---
+++ src/main/java/io/github/mongsil3344/ikeeper/bementoring/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.mongsil3344.ikeeper.bementoring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mongsil3344/ikeeper/bementoring/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mongsil3344.ikeeper.bementoring.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
